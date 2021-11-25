31b72c63506be7fa7fc58c92b6ad6ffa278ab6ec
Dzakfr1006 <16520278@std.stei.itb.ac.id>
2021-11-26 01:28:03 +0700

    progress on marketplace.pl

--- src/house.pl
+++ src/house.pl
@@ -6,50 +6,58 @@
 :- dynamic(emptyDiary/1).
 
 /* Central Time Control */
-currentSeason(spring).
+currentSeason(semi).
 currentDay(1).
-dayCap(24).
+
+/* init season */
+season(semi).
+season(panas).
+season(gugur).
+season(dingin).
+
 houseOpCode(0).
 emptyDiary(0).
 diaries([[0,'']]).
 
 checkDay(D):-
-    D >= 7, D < 13 -> retract(currentseason(spring)), asserta(currentseason(summer));
-    D >= 13, D < 19 -> retract(currentseason(summer)), asserta(currentseason(autumn));
-    D >= 19, D < 25 -> retract(currentseason(autumn)), asserta(currentseason(winter));
-    write('You have worked hard, but in the end result is all that matters. May God bless you in the future with kind people!').
+    D == 0, D < 7 -> currentSeason(X), write('musim: '), write(X);
+    D == 7 -> retract(currentSeason(_)), asserta(currentSeason(panas)), write('musim: panas');
+    D == 13 -> retract(currentSeason(_)), asserta(currentSeason(gugur)), write('musim: gugur');
+    D == 19 -> retract(currentSeason(_)), asserta(currentSeason(dingin)), write('musim: dingin');
+    D == 25 -> write('Kamu telah berusaha, namun sayang sekali kamu masih jauh dari kesuksesan. Silakan coba lagi dan tetap semangat!');
+    currentSeason(X), write('musim: '), write(X).
 
-house:- houseOpCode(1), write('You\'re already inside your house.\n'), !.
+house:- houseOpCode(1), write('Kamu sudah berada di dalam rumah.\n'), !.
 house:-
     houseOpCode(0),
     retract(houseOpCode(0)),asserta(houseOpCode(1)),
-    write('Welcome to the House! What would you like to do?\n'),
-    write('- sleep\n-writeDiary\n-readDiary\n-exit').
+    write('Tadaima. Apa yang ingin kamu lakukan?\n'),
+    write('- bobo\n- tulisDiary\n- bacaDiary\n- keluar').
 
 writeD:-
     currentDay(D),
     diaries(C),
-    write('Write your diary for Day '), write(D), write('\n'),
+    write('Tulis diary untuk hari ke-'), write(D), write('\n'),
     read(X),
     retract(diaries(C)), asserta(diaries([[0,X]|C])),
     retract(diaries([[0,X]|C])), asserta(diaries([[D,X]|C])),
     emptyDiary(A), 
     A =:= 0 -> retract(emptyDiary(0)),asserta(emptyDiary(1)).
 
-writeDiary:-
+tulisDiary:-
     currentDay(D),
     diaries([[Dc,_]|A]),
-    Dc =:= D -> write('Overwritting Diary...'), retract(diaries([[Dc,_]|A])), asserta(diaries(A)), writeD;
+    Dc =:= D -> write('Menimpa Diary...\n'), retract(diaries([[Dc,_]|A])), asserta(diaries(A)), writeD;
     writeD.
 
 showDiary(_,[[Day,_]|_]):- 
-    Day =:= 0, write('Entry not found!'), !.
+    Day =:= 0, write('Entri tidak ditemukan!'), !.
 showDiary(X,[[Day,Entry]|Tail]):-
-    X =:= Day -> write('Here is your entry for day 1:\n'), write(Entry);
+    X =:= Day -> write('Ini entri untuk hari ke-'), write(Day), write(':\n'), write(Entry);
     showDiary(X,Tail).
 
 showDiaryList([[_,_]|_]):- 
-    emptyDiary(A), A =:= 0, write('Diary is empty, How about writing a new entry?'), !.
+    emptyDiary(A), A =:= 0, write('Diary-mu kosong, mungkin kamu mau menulis entri baru?'), !.
 showDiaryList([[Day,_]|_]):- 
     Day =:= 0,
     read(X),
@@ -60,15 +68,13 @@ showDiaryList([[Day,_]|Tail]):-
     write('- Day '), write(Day), write('\n'),
     showDiaryList(Tail).
 
-readDiary:-
-    write('Here are the list of your entries:\n'),
+bacaDiary:-
+    write('Ini daftar diary-mu:\n'),
     diaries(Ds),
     showDiaryList(Ds).
 
-
-sleep:-
-    write('You went to sleep, have a nice dream.'),
-    retract(currentseason(autumn)),asserta(currentseason(winter)),
+bobo:-
+    write('Kamu memilih untuk bobo, mimpi indah ^_^.\n'),
     currentDay(X),
     Y is X+1,
     retract(currentDay(X)),asserta(currentDay(Y)),
--- src/marketplace.pl
+++ src/marketplace.pl
@@ -2,58 +2,68 @@
 :- include(quest).
 :- include(house).
 :- include(items).
+:- include(player).
 
 /* declare dynamic predicates */
 :- dynamic(marketState/2).
 :- dynamic(marketContent/3).
 :- dynamic(money/1).
 :- dynamic(exp/1).
-:- dynamic(currentseason/1).
+:- dynamic(inMarketState/1).
 
-/* init season */
-season(spring).
-season(summer).
-season(autumn).
-season(winter).
+inMarketState(0).
 
-currentseason(spring).
+springMarketCrop([turnip, potato, cucumber, cabbage]).
+summmerMarketCrop([tomato, corn, onion, pineapple]).
+autumnMarketCrop([carrot, eggplant, sweet_potato, green_paper]).
 
-springMarketContent([turnip, potato, cucumber, cabbage]).
-summmerMarketContent([tomato, corn, onion, pineapple]).
-autumnMarketContent([carrot, eggplant, sweet_potato, green_paper]).
-winterMarketContent([carrot, lettuce]). 
-
-showMarketContent(_,[]):- !.
-showMarketContent(X,[Head|Tail]):-
-    write(X),
-    write('. '),
-    write(Head),
-    write('\n'),
+showMarketCrop(_,[]):- !.
+showMarketCrop(X,[Head|Tail]):-
+    cropPurchasePrice(Head,Price),
+    write(X), write('. '), write(Head), write(' ('), write(Price), write(' golds'), write('\n'),
     Y is X+1,
-    showMarketContent(Y,Tail).
+    showMarketCrop(Y,Tail).
 
+showRanch:-
+    write('5. Chicken (golds'), write('\n'),
+    write('6. Sheep (golds'), write('\n'),
+    write('7. Cow (golds'), write('\n'),
+
+showTool:-
+    toolPurchasePrice(shovel,X).
+    toolPurchasePrice(fishing_rod,Y).
+    write('8. Shovel ('), write(X), write(' golds'), write('\n'),
+    write('9. fishing rod ('), write(Y), write(' golds'), write('\n'),
+
+pickItem:-
+    write('not implemented.')
 
 buy:-
-    write('What do you want to buy?\n'),
+    write('Barang yang ingin dibeli?\n'),
     currentseason(X),
-    write('Current Season: '),
+    write('Musim: '),
     write(X), write('\n'),
-    X == spring -> springMarketContent(Y), showMarketContent(1,Y);
-    X == summer -> summmerMarketContent(Y), showMarketContent(1,Y);
-    X == autumn -> autumnMarketContent(Y), showMarketContent(1,Y);
-    X == winter -> winterMarketContent(Y), showMarketContent(1,Y).
+    X == spring -> springMarketContent(Y), showMarketCrop(1,Y), showRanch, showTool, pickItem;
+    X == summer -> summmerMarketContent(Y), showMarketCrop(1,Y), showRanch, showTool, pickItem;
+    X == autumn -> autumnMarketContent(Y), showMarketCrop(1,Y), showRanch, showTool, pickItem;
+    X == winter -> winterMarketContent(Y), showMarketCrop(1,Y), showRanch, showTool, pickItem.
     
 
 sell:-
-    write('Here are the items in your inventory'), write('\n'),
+    write('Daftar item di dalam inventory'), write('\n'),
     write
-    write('What do you want to sell?'), write('\n'),
+    write('Barang yang ingin dijual?'), write('\n'),
     read(X),
     write(X), write('\n').
 
 market:-
+    retract(inMarketState(0)), asserta(inMarketState(1)),
     playerloc(Xp,Yp),
     marketplaceloc(Xm,Ym),
     (Xp =:= Xq, Yp =:= Yq) -> initMarket;
-    write('You are not on the Marketplace tile. \n').
-    write('What do you want to do?\n1. Buy\n2. Sell').
+    write('Kamu tidak sedang berada di Marketplace. \n').
+    write('Apa yang ingin kamu lakukan?\n1. Beli barang\n2. Jual barang').
+
+exitShop:-
+    write('Terima kasih sudah datang, silakan datang kembali!'),
+    retract(inMarketState(_)), asserta(inMarketState(0)),
