7a65016fad969b8dc847fa181153bb21365fba03
BDS_Hyper <haqibariza@gmail.com>
2021-11-26 19:30:15 +0700

    nggak ngeliat level.pl aeugh

--- src/activities.pl
+++ src/activities.pl
@@ -3,6 +3,7 @@
 :- include('inventory.pl').
 :- include('map.pl').
 :- include('items.pl').
+:- include('level.pl').
 
 
 /* Farming */
@@ -15,8 +16,7 @@ plant :- playerloc(X,Y), digloc(X,Y),write('You have:'), nl,
          write('you planted a '), write(Crop), write(' seed.'), retract(digloc(X,Y)).
 
 harvest :- playerloc(X,Y), croploc(X,Y,Crop,0), crop(Crop), addItem(Crop,1), write('you harvested '), write(Crop), write('.'), nl,
-           write('you gained 2 farming exp'), 
-           farmingexp(Farmer, Z), Z1 is X+2, asserta(farmingexp(Farmer, Z1)), retract(farmingexp(Farmer, Z)), retract(croploc(X,Y)).
+           addExpFarming(Farmer,2), retract(croploc(X,Y)).
 
 /* Fishing */
 
@@ -24,11 +24,10 @@ fish :- random(1,5,Z), getFishing(Z,X), fishing(X).
 
 fishing(Fishing) :- Fishing==fish, random(1,6,Y), getFish(Y,Fish),
                     write('You got '), write(Fish), write('!'), nl,
-                    write('You gained 10 fishing exp'), 
-                    fishingexp(Farmer, X),Z is X+10, asserta(fishingexp(Farmer, Z)), retract(fishingexp(Farmer, X);
+                    addExpFishing(Farmer,10);
                     Fishing==none,
                     write('You didn’t get anything!'), nl,
-                    write('You gained 5 fishing exp'), fishingexp(Farmer, Z),Z1 is X+5, asserta(fishingexp(Farmer, Z1)), retract(fishingexp(Farmer, Z)).
+                    addExpFishing(Farmer,5).
 
 /* Ranching */
 ranch :- playerloc(X,Y), ranchloc(X,Y),
@@ -47,20 +46,20 @@ ranching(Animal) :- animal(Animal), productYield(Animal,egg), ranchChicken;
 
 ranchChicken :- eggProduct(X), X\==0, , addItem(egg,X), write('Your chicken lays '), nl,
                 write(X), write('eggs.'), nl,
-                write('You gained 6 ranching exp!'), ranchingexp(Farmer, Z), Z1 is Z+6, asserta(ranchingexp(Farmer, Z1)), retract(ranchingexp(Farmer, Z));
+                addExpRanching(Farmer,6);
                 eggProduct(X), X==0, write('Your chicken hasn’t produced any egg.'), nl,
                 write('Please check again later.').
 
 ranchSheep :- woolProduct(X,_), X\==0, addItem(wool,X), write('Your sheep product '), nl,
               write(X), write('wools.'), nl,
-              print.('You gained 12 ranching exp!'), ranchingexp(Farmer, Z), Z1 is Z+12, asserta(ranchingexp(Farmer, Z1)), retract(ranchingexp(Farmer, Z));
+              addExpRanching(Farmer,12);
               woolProduct(X,_), X==0, write('Your sheep hasn’t produced any wool.'), nl,
               write('Please check again later.').
 
 
 ranchCow :- milkProduct(X), X\==0, addItem(milk,X), write('Your cow product '), nl,
             write(X), write('milks.'), nl,
-            write('You gained 18 ranching exp!'), ranchingexp(Farmer, Z), Z1 is Z+18, asserta(ranchingexp(Farmer, Z1)), retract(ranchingexp(Farmer, Z));
+            addExpRanching(Farmer,18);
             milkProduct(X), X==0, write('Your cow hasn’t produced any milk.'), nl,
             write('Please check again later.').
 
