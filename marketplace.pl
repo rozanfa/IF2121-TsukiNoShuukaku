a0dbd94076c7950fe50a200f4c9c4111f1cdd03d
Timothy Stanley <79507886+Stanley77-web@users.noreply.github.com>
2021-11-28 20:45:58 +0700

    oke nitip dulu males bngt conilct mulu

---
+++ marketplace.pl
@@ -0,0 +1,154 @@
+/* Include necessary modules */
+:- include('player.pl').
+:- include('time.pl').
+:- include('inventory.pl').
+:- include('map.pl').
+
+/* declare dynamic predicates */
+:- dynamic(marketState/2).
+:- dynamic(marketContent/3).
+:- dynamic(money/1).
+:- dynamic(exp/1).
+:- dynamic(inMarketState/1).
+:- dynamic(inAlchemitsState/1).
+:- dynamic(potionEfect/2).
+:- dynamic(tempLevel/4).
+:- dynamic(tempList/1).
+:- dynamic(shovellevel/1).
+:- dynamic(fishing_rodlevel/1).
+
+potionEfect(0,0).
+tempLevel(0,0,0,0).
+
+shovellevel(1).
+fishing_rodlevel(1).
+
+upTool(X) :- X == shovel -> shovellevel(Y), Ym is Y+1, retract(shovellevel(Y)), asserta(shovellevel(Ym));
+             X == fishing_rod -> fishing_rodlevel(Y), Ym is Y+1, retract(fishing_rodlevel(Y)), asserta(fishing_rodlevel(Ym));
+
+inMarketState(0).
+inAlchemitsState(0).
+tempList([]).
+
+springMarketCrop([turnip_seed, potato_seed, cucumber_seed, cabbage_seed]).
+summmerMarketCrop([tomato_seed, corn_seed, onion_seed, pineapple_seed]).
+autumnMarketCrop([carrot_seed, eggplant_seed, spinach_seed, pumpkin_seed]).
+
+alchemitsPotion([farming_potion,fishing_potion,ranching_potion,stamina_potion,all_potion]).
+
+/* Normal */
+formList([A,B,C,D]):-
+    retract(tempList(_)), asserta(tempList([A,B,C,D,chicken,cow,sheep,shovel,fishing_rod])).
+
+showList(_,[]):- !.
+showList(X,[Head|Tail]):-
+    X >= 10 -> potionPrice(Head,Pr) ,No is X - 9, write(No), write('. '), mkstr(Head,Str), write(Str), write(' ('), write(Pr), write(' golds)\n'), Xn is X+1, showList(Xn,Tail);
+    X >= 8, X < 10, shovellevel(P), fishing_rodlevel(Q), write('8. Sekop Level '), write(P), write(' (300 golds)\n9. Alat Pancing Level '), write(Q), write(' (500 golds)\n');
+    X >= 1, X < 5, cropPurchasePrice(Head,Pr), write(X), write('. '), mkstr(Head,Str), write(Str), write(' ('), write(Pr), write(' golds)\n'), Xn is X+1, showList(Xn,Tail);
+    X >= 5, X < 8, animalPrice(Head,Pr), write(X), write('. '), mkstr(Head,Str), write(Str), write(' ('), write(Pr), write(' golds)\n'), Xn is X+1, showList(Xn,Tail).
+
+/* Winter */
+formListWinter:-
+    retract(tempList(_)), asserta(tempList([chicken,cow,sheep,shovel,fishing_rod])).
+showListWinter(_,[]):- !.
+showListWinter(X,[Head|Tail]):-
+    X >= 4 -> shovellevel(P), fishing_rodlevel(Q), write('8. Sekop Level '), write(P), write(' (300 golds)\n9. Alat Pancing Level '), write(Q), write(' (500 golds)\n\n');
+    animalPrice(Head,Pr), write(X), write('. '), mkstr(Head,Str), write(Str), write(' ('), write(Pr), write(' golds)\n'), Xn is X+1, showListWinter(Xn,Tail).
+
+pickfromTemp(_,[],Item):- Item is nan, !.
+pickfromTemp(X,_,Item):- X < 0, Item is nan, !.
+pickfromTemp(X,[Head|_],Head):- Xm is X-1, Xm =:= 0, !.
+pickfromTemp(X,[_|Tail],Item):-
+    Xm is X-1, pickfromTemp(Xm,Tail,Item).
+
+processItem(Item,Pr,Am):-
+    Total is Pr * Am,
+    gold(_,X),
+    Am < 0 -> write('Jumlah tidak valid, ulang kembali masukan!\n'), read(Am), processItem(Item,Pr,Am);
+    X < Total -> write('Jumlah gold kamu tidak cukup untuk membeli barang ini!\n\n'), buy;
+    tool(Item) -> upTool(Item), buy;
+    GLeft is X - Total,
+    retract(gold(Player,X)), asserta(gold(Player,GLeft)),
+    addItem(Item,Am), mkstr(Item,Str),
+    write('Kamu berhasil membeli '), write(Am), write(' '), write(Str), write('.\nKamu membayar sebesar '), write(Total), write(' gold\n\n'), buy.
+
+pickItem:-
+    read(X),
+    tempList(List),
+    pickfromTemp(X,List,Item), C is X,
+    (C >= 1, C < 5 -> cropPurchasePrice(Item,Pr), write('Berapa banyak yang ingin kamu beli?\n'), read(Am), processItem(Item,Pr,Am);
+    C >= 5, C < 8 -> animalPrice(Item,Pr), write('Berapa banyak yang ingin kamu beli?\n'), read(Am), processItem(Item,Pr,Am);
+    C >= 8 ->  toolPurchasePrice(Item,Pr), processItem(Item,Pr,1);
+    write('Item tidak ditemukan.\n\n'), buy).
+
+buy:-
+    write('Barang yang ingin dibeli?\n'),
+    season(X),
+    write('Musim: '), isSeason(NamaMusim,X),
+    write(NamaMusim), nl,
+    X =:= 1 -> springMarketCrop(Y), formList(Y), tempList(List), showList(1,List), pickItem;
+    X =:= 2 -> summmerMarketCrop(Y), formList(Y), tempList(List), showList(1,List), pickItem;
+    X =:= 3 -> showRanch, autumnMarketCrop(Y), tempList(List), showList(1,List), pickItem;
+    X =:= 4 -> formListWinter, showListWinter, write('\n(Tidak ada tanaman yang sedang dijual.)\n'), pickItem.
+
+sell:-
+    write('Daftar item di dalam inventory'), nl,
+    write('Barang yang ingin dijual?'), nl,
+    read(X),
+    write(X), nl.
+
+market:-
+    retract(inMarketState(0)), asserta(inMarketState(1)),
+    playerloc(Xp,Yp),
+    marketplaceloc(Xm,Ym),
+    (Xp =:= Xm, Yp =:= Ym) -> write('Apa yang ingin kamu lakukan?\n1. (buy) Beli barang\n2. (sell) Jual barang');
+    write('Kamu tidak sedang berada di Marketplace. \n').
+
+exitShop:-
+    write('Terima kasih sudah datang, silakan datang kembali!'),
+    retract(tempList(_)), asserta(tempList([])),
+    retract(inMarketState(_)), asserta(inMarketState(0)).
+
+alchemits:-
+    (alchemitslocc(Xa,Ya,TP,_), playerloc(Xp,Yp),
+    ((TP > 0, Xp =:= Xa, Yp =:= Ya) ->
+        retract(inAlchemitsState(_)), asserta(inAlchemitsState(1)),
+        write('Ohh tidak kamu menemukanku!!!\nSilakan pilih potoin yang ingin dibeli\n1. (buyPotion) Beli Potion');
+        write('Tidak terjadi apa-apa....'))),!.
+
+exitAlchemist:-
+    write('Jangan bilang siapa-siapa tentang kehadiranku!'),
+    retract(tempList(_)), asserta(tempList([])),
+    retract(inAlchemitsState(_)), asserta(inAlchemitsState(0)).
+
+buyPotion:-
+    inAlchemitsState(X),
+        X =:= 1 ->
+        write('Silakan Potion yang ingin dipilih :\n'),
+            alchemitsPotion(Y), retract(tempList(_)),
+            asserta(tempList(Y)), tempList(List), showList(10,List),
+            read(Z), setPotion(Z),!;
+    true.
+
+setPotion(X) :-
+    potionEfect(P,_),
+        P =:= 0 ->
+           (X =:= 1 -> retract(potionEfect(_,_)), asserta(potionEfect(1,10)),
+                       retract(farminglevel(_,Y)), asserta(farminglevel(_,3)), asserta(tempLevel(Y,_,_,_)),
+                       write('Potion Farming sudah aktif level farming menjadi level maximal');
+            X =:= 2 -> retract(potionEfect(_,_)), asserta(potionEfect(2,10)),
+                       retract(fishinglevel(_,Y)), asserta(fishinglevel(_,3)), asserta(tempLevel(_,Y,_,_)),
+                       write('Potion Fishing sudah aktif level fishing menjadi level maximal');
+            X =:= 3 -> retract(potionEfect(_,_)), asserta(potionEfect(3,10)),
+                       retract(ranchinglevel(_,Y)), asserta(ranchinglevel(_,3)), asserta(tempLevel(_,_,Y,_)),
+                       write('Potion Ranching sudah aktif level ranching menjadi level maximal');
+            X =:= 4 -> retract(potionEfect(_,_)), asserta(potionEfect(4,10)),
+                       retract(maxStamina(_,Y)), asserta(maxStamina(_,99)), asserta(tempLevel(_,_,_,Y)),
+                       write('Potion Stamina sudah aktif stamina menjadi 99');
+            X =:= 5 -> retract(potionEfect(_,_)), asserta(potionEfect(5,10)),
+                       retract(farminglevel(_,Y)), asserta(farminglevel(_,3)),
+                       retract(fishinglevel(_,Z)), asserta(fishinglevel(_,3)),
+                       retract(ranchinglevel(_,V)), asserta(ranchinglevel(_,3)),
+                       retract(maxStamina(_,W)), asserta(maxStamina(_,99)), asserta(tempLevel(Y,Z,V,W)),
+                       write('Potion God sudah aktif (Efek sama seperti menggunakan keempat potion)')),!;
+        write('Hanya bisa menggunakan satu potion'),!.
---
+++ time.pl
@@ -0,0 +1,139 @@
+/* declare dynamic predicates */
+:- dynamic(time/1).
+:- dynamic(day/1).
+:- dynamic(season/1).
+:- dynamic(weather/1).
+
+
+/* Musim */
+isSeason('Semi',1).
+isSeason('Panas',2).
+isSeason('Gugur',3).
+isSeason('Dingin',4).
+isSeason('Semi',5).
+
+/* Init time */
+time(0).
+day(0).
+season(1).
+
+
+/* Weather */
+setSpringWeather(X) :-  X = 1 -> (retract(weather(_)), asserta(weather('cerah')));
+                        X = 2 -> (retract(weather(_)), asserta(weather('cerah')));
+                        X = 3 -> (retract(weather(_)), asserta(weather('berawan')));
+                        X = 4 -> (retract(weather(_)), asserta(weather('berawan')));
+                        X = 5 -> (retract(weather(_)), asserta(weather('hujan')));
+                        X = 6 -> (retract(weather(_)), asserta(weather('hujan'))). %Buffer
+
+setSummerWeather(X) :-  X = 1 -> (retract(weather(_)), asserta(weather('cerah')));
+                        X = 2 -> (retract(weather(_)), asserta(weather('cerah')));
+                        X = 3 -> (retract(weather(_)), asserta(weather('cerah')));
+                        X = 4 -> (retract(weather(_)), asserta(weather('berawan')));
+                        X = 5 -> (retract(weather(_)), asserta(weather('berawan')));
+                        X = 6 -> (retract(weather(_)), asserta(weather('hujan'))). %Buffer
+
+setFallWeather(X) :-    X = 1 -> (retract(weather(_)), asserta(weather('cerah')));
+                        X = 2 -> (retract(weather(_)), asserta(weather('berawan')));
+                        X = 3 -> (retract(weather(_)), asserta(weather('berangin')));
+                        X = 4 -> (retract(weather(_)), asserta(weather('berangin')));
+                        X = 5 -> (retract(weather(_)), asserta(weather('hujan')));
+                        X = 6 -> (retract(weather(_)), asserta(weather('hujan'))). %Buffer
+
+setWinterWeather(X) :-  X = 1 -> (retract(weather(_)), asserta(weather('salju')));
+                        X = 2 -> (retract(weather(_)), asserta(weather('salju')));
+                        X = 3 -> (retract(weather(_)), asserta(weather('salju')));
+                        X = 4 -> (retract(weather(_)), asserta(weather('cerah')));
+                        X = 5 -> (retract(weather(_)), asserta(weather('cerah')));
+                        X = 6 -> (retract(weather(_)), asserta(weather('cerah'))). %Buffer
+
+
+/* Init weather */
+weather('cerah').
+
+
+/*DUMMY*/
+:- dynamic(cropLoc/4).
+:- dynamic(readyCropLoc/4).
+cropLoc(1, 2, aaa, 3).
+cropLoc(2, 2, bbb, 3).
+cropLoc(3, 2, ccc, 3).
+/*DUMMY*/
+
+
+addTime :-  retract(time(CurrTime)),
+            CurrTime < 23 -> NewTime is CurrTime + 1,  asserta(time(NewTime)), writeTime, decreasePotion;
+            CurrTime = 23 -> NewTime is 1, asserta(time(NewTime)),writeTime, addDay, checkCrops, setAlchemist.
+
+checkCrops :-   forall(cropLoc(X, Y, CropName, HarvestTime),
+                ((day(CurrDay), HarvestTime == CurrDay),
+                retract(cropLoc(X, Y, CropName, HarvestTime)),
+                asserta(readyCropLoc(X, Y, CropName, HarvestTime)))); true.
+
+
+addDay :-   (retract(day(CurrDay)), NewDay is CurrDay + 1,
+            asserta(day(NewDay))), randomizeWeather,
+           ( NewDay == 24 -> finishGame;
+            S is mod(NewDay,6),  S == 0 -> addSeason;
+            true), writeDay, true.
+
+setAlchemist :- alchemitsloc(X,Y,TP,TC), retract(alchemitsloc(X,Y,TP,TC)),
+            (TC > 0 ->
+                NTC is TC - 1,
+                (NTC =:= 0 ->
+                    NTP is 2, NTC is 0;
+                    NTP is TP);
+             TP > 0 ->
+                NTP is TP - 1,
+                (NTP =:= 0 ->
+                    NTC is 6, NTP is 0;
+                    NTC is TC)),
+                    asserta(alchemitsloc(X,Y,NTP,NTC)).
+
+decreasePotion :- potionEfect(E,ST),
+        E =\= 0 -> NST is ST - 1, retract(potionEfect(E,ST)),
+            (NST =\= 0 ->
+                 asserta(potionEfect(E,NST));
+                 (E =:= 1 -> retract(tempLevel(X,_,_,_)), retract(farminglevel(_,_)),
+                             asserta(tempLevel(0,0,0,0)), asserta(farminglevel(_,X));
+                  E =:= 2 -> retract(tempLevel(_,X,_,_)), retract(fishinglevel(_,_)),
+                             asserta(tempLevel(0,0,0,0)), asserta(fishinglevel(_,X));
+                  E =:= 3 -> retract(tempLevel(_,_,X,_)), retract(ranchinglevel(_,_)),
+                             asserta(tempLevel(0,0,0,0)), asserta(ranchinglevel(_,X));
+                  E =:= 4 -> retract(tempLevel(_,_,_,X)), retract(maxStamina(_,_)),
+                             asserta(tempLevel(0,0,0,0)), asserta(maxStamina(_,X));
+                  E =:= 5 -> retract(tempLevel(X,Y,Z,W)), asserta(tempLevel(0,0,0,0)),
+                             retract(farminglevel(_,_)), asserta(farminglevel(_,X)),
+                             retract(fishinglevel(_,_)), asserta(fishinglevel(_,Y)),
+                             retract(ranchinglevel(_,_)), asserta(ranchinglevel(_,Z)),
+                             retract(maxStamina(_,_)), asserta(maxStamina(_,W))
+                 ), asserta(potionEfect(0,0));
+             NST =:= 0 -> write('Efek potion sudah menghilang.')
+            );
+        true. 
+
+addSeason :-    retract(season(CurrSeason)),
+                NewSeason is CurrSeason + 1, asserta(season(NewSeason)).
+
+writeDay :-    ( write('-----------------------------------------------------------------------'), nl,
+                write('----------------------------- Musim '), season(CurrSeason), isSeason(SeasonName, CurrSeason), write(SeasonName), write(' -----------------------------'),nl,
+                write('------------------------------ Hari Ke-'), day(CurrDay), write(CurrDay), write(' ------------------------------'),nl,
+                write('---------------------------- Cuaca : '), weather(CurrWeather), write(CurrWeather), write(' ----------------------------'), nl,
+                write('-----------------------------------------------------------------------'), nl, true, !).
+
+writeTime :- write('Waktu sekarang : '), time(CurrTime), write(CurrTime), write('/24.'), nl.
+
+finishGame :-   gold(_, currentGold),
+                currentGold >= 20000 ->
+                write('Kamu telah berusaha, namun sayang sekali kamu masih jauh dari kesuksesan. \nSilakan coba lagi dan tetap semangat!'), halt;
+                currentGold < 20000 ->
+                write('Selamat! Kamu akhirnya berhasil mengumpulkan 20000 gold!\n'), halt.
+
+randomizeWeather :- season(S), random(1,6,X), setWeather(S, X).
+
+setWeather(S, X) :- season(S),
+                    S =:= 1 -> (setSpringWeather(X));
+                    S =:= 2 -> (setSummerWeather(X));
+                    S =:= 3 -> (setFallWeather(X));
+                    S =:= 4 -> (setWinterWeather(X));
+                    S =:= 5 -> (setSpringWeather(X)).
